6285f6f1766114f4c8c0e147948b101fa3addba3
Gabriel Spencer <gabrielspencer@gmail.com>
2022-06-20 16:08:27 -0700

    Copy gradle files to prepare for splitting sproute repo

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+rootProject.name = "path-to-plunder"
+include(
+    "kexp:petals:annotations",
+    "kexp:petals:post-processing-tests",
+    "kexp:petals:processor",
+    "kexp:sproute:annotations",
+    "kexp:sproute:processor",
+    "kexp:sproute:post-processing-tests",
+    "kexp:kexportable:annotations",
+    "kexp:kexportable:processor",
+    "kexp:kexportable:post-processing-tests")
