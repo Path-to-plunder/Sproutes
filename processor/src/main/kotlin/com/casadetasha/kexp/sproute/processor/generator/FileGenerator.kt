8d4e38923348e777b8957d588b779f148ed60e29
Gabriel Spencer <gabrielspencer@gmail.com>
2022-06-29 00:42:16 -0700

    Clean up code from time test

--- processor/src/main/kotlin/com/casadetasha/kexp/sproute/processor/generator/FileGenerator.kt
+++ processor/src/main/kotlin/com/casadetasha/kexp/sproute/processor/generator/FileGenerator.kt
@@ -4,18 +4,11 @@ import com.casadetasha.kexp.annotationparser.AnnotationParser.printNote
 import com.casadetasha.kexp.generationdsl.dsl.FileTemplate.Companion.generateFile
 import com.casadetasha.kexp.sproute.processor.generator.template.generateConfigureSproutesFunction
 import com.casadetasha.kexp.sproute.processor.generator.tree.SprouteTree
-import com.google.common.base.Stopwatch
-import java.util.concurrent.TimeUnit
 
 internal class FileGenerator(
     private val kaptKotlinGeneratedDir: String,
     private val sprouteTree: SprouteTree
 ) {
-
-    companion object {
-        const val SPROUTE_CONFIG_FILE_NAME = "Sproutes"
-        const val ROUTING_PACKAGE_NAME = "com.casadetasha.kexp.sproute"
-    }
 
     internal fun generateSproutes() {
         printNote("Generating sproute hierarchies")
@@ -31,11 +24,8 @@ internal class FileGenerator(
         }.writeToDisk()
     }
 
-    private fun time(function: () -> Unit): Long {
-        val stopwatch = Stopwatch.createStarted()
-        function()
-        stopwatch.stop()
-
-        return stopwatch.elapsed(TimeUnit.MILLISECONDS)
+    companion object {
+        const val SPROUTE_CONFIG_FILE_NAME = "Sproutes"
+        const val ROUTING_PACKAGE_NAME = "com.casadetasha.kexp.sproute"
     }
 }
